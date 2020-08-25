d505c99bf83321c4dbb4afe0f354cf1fe29adfcc
Max Peterson <maxpeterson00@gmail.com>
2020-08-25 11:32:44 -0700

    updated browsewr config for font loading

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
-import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
 import { faGithub, faStrava, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
@@ -10,10 +8,7 @@ import Header from "components/header"
 import Footer from "components/footer"
 import GlobalStyle from "styles/global-style"
 
-/* In addition to manually importing style sheet above, fixes sixing bug when icons are being loaded... */
-config.autoAddCss = false
-
-/* These icons can now be accessed by string reference elsewhere. Useful for getting icon data from external source. Could also import entire fab library... */
+// These icons can now be accessed by string reference elsewhere. Useful for getting icon data from external source. Could also import entire fab library...
 library.add(faPaperPlane, faLinkedinIn, faGithub, faStrava)
 
 export default function Layout({ page, children }) {
