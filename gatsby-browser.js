8f67fdc5e3e693543184c3190efbe69255a78101
Max Peterson <maxpeterson00@gmail.com>
2020-08-25 11:35:03 -0700

    updated browser config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-require("typeface-cooper-hewitt");
-require("typeface-roboto");
-
 // Fixes sixing bug when icons are being loaded...
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false
+
+require("typeface-cooper-hewitt");
+require("typeface-roboto");
